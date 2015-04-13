327cb3aad04c5281c12c4f9edb18d57722236e51
Marcelo Toledo <marcelo@marcelotoledo.com>
2015-04-12 22:50:28 -0300

    nhac

--- folha.py
+++ folha.py
@@ -25,25 +25,10 @@
 #  WOLF      PRETO
 #  KUSHIDA   AZUL
 
-COGU   = ({ 'cometa': 'halley',  'grupo': 'amarelo'  },
-          { 'cometa': 'encke',   'grupo': 'vermelho' },
-          { 'cometa': 'wolf',    'grupo': 'preto'    },
-          { 'cometa': 'kushida', 'grupo': 'azul'     })
-
-DEPARA = { 'a':  1, 'b':  2, 'c':  3, 'd':  4, 'e':  5, 'f':  6, 
-           'g':  7, 'h':  8, 'i':  9, 'j': 10, 'k': 11, 'l': 12, 
-           'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 
-           's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 
-           'y': 25, 'z': 26 }
-
-def translate_char(char):
-    return DEPARA[char]
+import string
 
 def translate(str):
-    return reduce((lambda x, y: x * y), map(translate_char, list(str)))
+    return reduce((lambda x, y: x * y), map((lambda char: string.ascii_lowercase.index(char) + 1), list(str)))
 
-for i in COGU:
-    print('Cometa: %15s     -> %15d'   % (i['cometa'],            translate(i['cometa'])))
-    print('Grupo:  %15s     -> %15d'   % (i['grupo'],             translate(i['grupo'])))
-    print('Cometa: %15d / 45 = %15d'   % (translate(i['cometa']), translate(i['cometa']) % 45))
-    print('Grupo:  %15d / 45 = %15d\n' % (translate(i['grupo']),  translate(i['grupo']) % 45))
+for cometa, grupo in [('halley','amarelo'), ('encke','vermelho'), ('wolf','preto'), ('kushida','azul')]:
+    print('Cometa %10s (%10d / 45 = %d) \tGrupo: %10s (%10d / 45 = %d)' % (cometa, translate(cometa), translate(cometa) % 45, grupo, translate(grupo), translate(grupo) % 45))
